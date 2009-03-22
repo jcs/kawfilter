e0007c3aefadd9935fee72e6d8d094ec5e850a23
jcs <jcs>
2009-03-22 18:44:08 +0000

    add license

--- kawfilter.user.js
+++ kawfilter.user.js
@@ -1,5 +1,5 @@
-// $Id: kawfilter.user.js,v 1.2 2009/03/22 18:37:15 jcs Exp $
-
+// $Id: kawfilter.user.js,v 1.3 2009/03/22 18:44:08 jcs Exp $
+//
 // ==UserScript==
 // @name           kawfilter
 // @namespace      http://jcs.org/code/
@@ -8,8 +8,34 @@
 // @include        http://forums.wayot.org/other/pages/*
 // @include        http://f.wayot.org/other/
 // @include        http://f.wayot.org/other/pages/*
-// @author         jcs@jcs.org
+// @author         joshua stein <jcs@jcs.org>
 // ==/UserScript==
+//
+// Copyright (c) 2008-2009 joshua stein <jcs@jcs.org>
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+// 
+// 1. Redistributions of source code must retain the above copyright
+//    notice, this list of conditions and the following disclaimer.
+// 2. Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in the
+//    documentation and/or other materials provided with the distribution.
+// 3. The name of the author may not be used to endorse or promote products
+//    derived from this software without specific prior written permission.
+// 
+// THIS SOFTWARE IS PROVIDED BY THE AUTHOR "AS IS" AND ANY EXPRESS OR
+// IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+// OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+// IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+// NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+// THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+// THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
 
 // both of these searches are case insensitive and may contain regexp wildcards
 // so be sure to escape regexp metachars (such as \*+?|{[()]^$.) with a
